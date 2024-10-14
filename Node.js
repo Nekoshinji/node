6ff9c388b1e914762b1c7f766e9922ea9287bda9
Nekoshinji <obito26@gmail.com>
2024-10-14 17:11:25 +0200

    Modif des fichier Node.js, .env.sample et server.js supprimer

--- Node.js
+++ Node.js
@@ -1,6 +1,7 @@
-const MY_NAME = "Seb";
-const MY_CITY = "Lyon";
-const MY_LANGUAGE = "French";
+import "dotenv/config"; // this line is mandatory when you want to read variables from the '.env' file
+const MY_NAME = process.env.MY_NAME;
+const MY_CITY = process.env.MY_CITY;
+const MY_LANGUAGE = process.env.MY_LANGUAGE;
 
 console.log(
 	"I am " + MY_NAME + ", wilder in " + MY_CITY + ", and I love " + MY_LANGUAGE,
